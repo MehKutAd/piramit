18cd720f8a2bbf021add4ff8ea6cf37ebfe86a25
Muhammed Ali Aktaş <mhmmdlkts@gmail.com>
2020-04-24 15:22:19 +0200

    Giris bilgisi geliştirildi

--- src/piramit.java
+++ src/piramit.java
@@ -5,10 +5,11 @@ public class piramit {
 
     public static void main(String[] args) {
         Scanner scanIn= new Scanner(System.in);
-        System.out.print("satır sayısı giriniz: ");
+        System.out.print("Satır sayısı giriniz: ");
         int a = scanIn.nextInt();
         islem(a);
     }
+    
     public static void islem(int rews) {
         for(int i = 1; i <= rews ; i++) {
             for(int j = 1; j <= i; j++) {

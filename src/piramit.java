c968232e98812e3e66c5d3c43b69f05777f46a1d
MehKutAd <kutayadamlk@gmail.com>
2020-04-25 00:39:15 +0300

    at center

--- src/piramit.java
+++ src/piramit.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import javax.swing.*;
 import java.util.Scanner;
 
@@ -5,17 +7,20 @@ public class piramit {
 
     public static void main(String[] args) {
         Scanner scanIn= new Scanner(System.in);
-        System.out.print("Satır sayısı giriniz: ");
-        int a = scanIn.nextInt();
-        islem(a);
-        alt(a);
+        while (true) {
+            System.out.print("Satır sayısı giriniz: ");
+            int a = scanIn.nextInt();
+            islem(a);
+            alt(a);
+        }
+
     }
 
     public static void islem(int rews) {
         for(int i = 1; i <= rews ; i++) {
-            bosluk(rews-i);
+            bosluk(rews-i+35);
             for(int j = 1; j <= i; j++) {
-                System.out.print("* ");
+                System.out.print("*   ");
             }
             System.out.println();
         }
@@ -23,14 +28,15 @@ public class piramit {
 
     public static void bosluk(int count) {
         for(int i=1; i <= count ; i++)
-            System.out.print(" ");
+            System.out.print("  ");
     }
     public static void alt(int count) {
         for(int i=count-1; i >= 1 ; i--) {
-            bosluk(count-i);
+            bosluk(count-i+35);
             for (int j= i; j >= 1 ; j--)
-                System.out.print("* ");
+                System.out.print("*   ");
             System.out.println();
         }
     }
+
 }
